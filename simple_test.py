485512428aa8eaee88b68eb1531521fb1cbe9901
zacharskim <matthew.zacharski@gmail.com>
2024-11-18 22:08:14 -0500

    messing around with playwright

--- simple_test.py
+++ simple_test.py
@@ -1,46 +1,44 @@
-from playwright.sync_api import sync_playwright
+from playwright.async_api import async_playwright
 import time
+import asyncio
 
-def main():
+async def main():
     # Start Playwright
-    with sync_playwright() as p:
+    async with async_playwright() as p:
         # Launch Chromium in headed mode
-        browser = p.chromium.launch(headless=False, slow_mo=100)
+        browser = await p.chromium.launch(headless=False, slow_mo=100)
         
         # Open a new page
-        page = browser.new_page()
+        page = await browser.new_page()
         
         # Navigate to a website
-        page.goto("https://google.com")
-
-        # try:
-        #     page.locator("button:has-text('Accept all')").click()
-        # except:
-        #     pass  # Skip if no cookie popup appears
-        
+        await page.goto("https://streeteasy.com/")
 
         # Perform a search
         # page.locator("input[name='q']").wait_for()
-        page.locator("textarea[name='q']").fill("streeteasy")
+        # await page.locator("textarea[name='q']").fill("streeteasy")
             
         # (Optional) Submit the form by pressing 'Enter'
-        page.locator("textarea[name='q']").press("Enter")
+        # await page.locator("textarea[name='q']").press("Enter")
+        # await page.click('g-raised-button:has-text("Not now")') 
         
         
         # Wait for the results to load
-        page.wait_for_selector("h3")  # Wait for search result headings to appear
+        # await page.wait_for_selector("h3")  # Wait for search result headings to appear
 
         # Click the first search result
-        page.locator("h3").first.click()
+        # await page.locator("h3").first.click()
+
+        await page.get_by_text('Choose neighborhoods or boroughs').dblclick();
         
         # Take a screenshot of the resulting page
-        page.screenshot(path="screenshot_from_streeteasy.png")
+        # page.screenshot(path="screenshot_from_streeteasy.png")
         print("Screenshot saved as screenshot_from_streeteasy.png")
 
-        time.sleep(30)
+        time.sleep(4)
         #huh this kinda works?? unclear tho spend some more time in the future...
         
         
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
