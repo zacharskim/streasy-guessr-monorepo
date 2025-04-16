442bbf8279027e33afc63d1307cc48264ae95aee
zacharskim <matthew.zacharski@gmail.com>
2025-04-16 16:37:23 -0400

    added privacy page

---
+++ src/app/(with-header)/privacy/page.tsx
@@ -0,0 +1,8 @@
+export default function PrivacyPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Privacy works!</h1>
+      <p>This is the placeholder for the Privacy page.</p>
+    </div>
+  );
+}
